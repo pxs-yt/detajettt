10f960c356f75442eb4cbac418a61976559544cf
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-04-17 18:09:41 +0200

    PLIDA B2

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById("plida-criteri-valutazione-prove-scrivere").onclick = function () {
+    location.href = "https://pxs-yt.github.io/detajettt/PLIDA_B2/PLIDA-B2-Criteri-valutazione-prove-Scrivere.pdf"
+}
+
+document.getElementById("plida-criteri-valutazione-prove-parlare").onclick = function () {
+    location.href = "https://pxs-yt.github.io/detajettt/PLIDA_B2/PLIDA-B2-Criteri-valutazione-prove-Parlare.pdf"
+}
+
+document.getElementById("quaderno-delle-specifiche-p_b2").onclick = function () {
+    location.href = "https://pxs-yt.github.io/detajettt/PLIDA_B2/Quaderno-delle-specifiche-PLIDA-B2.pdf"
+}
+
+document.getElementById("prova-scrivere-commentata-p_b2").onclick = function () {
+    location.href = "https://pxs-yt.github.io/detajettt/PLIDA_B2/Prova-Scrivere-commentata_PLIDA-B2.pdf"
+}
+
+document.getElementById("p_b2-ascleg").onclick = function () {
+    location.href = "https://pxs-yt.github.io/detajettt/PLIDA_B2/PLIDA-B2-prova_esempio-ascleg.pdf"
+}
+
+document.getElementById("p_b2-parlare").onclick = function () {
+    location.href = "https://pxs-yt.github.io/detajettt/PLIDA_B2/PLIDA-B2-prova_esempio-parlare.pdf"
+}
+
+document.getElementById("p_b2-scrivere").onclick = function () {
+    location.href = "https://pxs-yt.github.io/detajettt/PLIDA-B2-prova_esempio-scrivere.pdf"
+}
+
+document.getElementById("p_b2-audio").onclick = function () {
+    location.href = "https://pxs-yt.github.io/detajettt/B2-nuovo_formato-Audio-esempio.mp3"
+}
