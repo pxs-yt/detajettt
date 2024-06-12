60e428cba80d7eb8f7415e271da38fa69e6a5894
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-06-12 22:48:35 +0200

    Added a heart emoji cursor

--- brewser.js
+++
@@ -1,8 +0,0 @@
-// Include the Brewser library
-<script type="text/javascript" src="/path/to/brewser.js"></script>
-
-// Use the library to detect the device
-Brewser.init();
-console.log("Operating System:", Brewser.device.os);
-console.log("Device Type:", Brewser.device.type);
-console.log("Browser:", Brewser.browser.type);
